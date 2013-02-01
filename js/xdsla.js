f5b8172d1aef584c031c225e4f38ad2786b00d93
Ed Henderson <ehenderson@sugarinc.com>
2013-01-31 17:39:49 -0800

    Improved JS and added interval to refresh the data (untested)
    - renamed pink
    - remove useless comments
    - rewrote JS to be much cleaner (still to test as interval is 1s and
    alert box present)

--- js/xdsla.js
+++ js/xdsla.js
@@ -1,27 +1,38 @@
-$(function() {
+var XDSLA = {
 
-    var last_accident = '01/29/2013'; //mm/dd/yyyy
-    var person = 'Dino';
-    
-    var days = days_between_today(last_accident);
-    $('#js-number-of-days-current-number').text(days);
-    $('#js-number-of-days-current-units').text((days == 1) ? 'day' : 'days');
-    $('#js-person-to-blame').text(person);
-    $('.shout').bigtext();
+    ONE_DAY_MS: 86400000,
+    last_accident: '01/29/2013',
+    blame_person: 'Dino',
+    update_interval_ms: 1000,
 
+    init: function() {
+        startInterval();
+    },
+
+    start_interval: function() {
+        update_dashboard();
+        var interval_id = setInterval(update_dashboard, update_interval_ms);
+    },
+
+    today_day_difference: function (date, ignoreToday) {
+        var today_ms = new Date().getTime();
+        var date_ms = Date.parse(date);
+        var difference_ms = Math.abs(today_ms - d_ms);
+        return Math.floor(difference_ms / ONE_DAY_MS) - (ignoreToday ? 1 : 0);
+    },
+
+    update_dashboard: function() {
+        var days = today_day_difference(last_accident, true);
+        $('#js-number-of-days-current-number').text(days);
+        $('#js-number-of-days-current-units').text((days == 1) ? 'day' : 'days');
+        $('#js-person-to-blame').text(blame_person);
+        $('.shout').bigtext();
+        alert('yes');
+    }
+
+};
+
+// entry point
+$(function () {
+    XDSLA.init();
 });
-
-function days_between_today (date) {
-
-    // The number of milliseconds in one day
-    var ONE_DAY = 1000 * 60 * 60 * 24;
-
-    var today_ms = new Date().getTime();
-    var d_ms = Date.parse(date);
-
-    // Calculate the difference in milliseconds
-    var difference_ms = Math.abs(today_ms - d_ms);
-
-    // Convert back to days and return
-    return Math.floor(difference_ms/ONE_DAY) - 1;
-}
