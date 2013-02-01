f9da10311746c7d2296812ca433ba42995c37c13
Ed Henderson <ehenderson@sugarinc.com>
2013-02-01 09:44:00 -0800

    JS Date Calculation tweak
    - removed today parameter as its useless
    - rewrote the function to flatten the time to 0 so we are dealing in
    absolute days
    - hardwire the fact that we don't count the day of the accident
    - special case, i.e. if the date of the accident we don't want to show
    -1, so show 0

--- js/xdsla.js
+++ js/xdsla.js
@@ -14,15 +14,29 @@ var XDSLA = {
         var interval_id = setInterval(XDSLA.update_dashboard, XDSLA.update_interval_ms);
     },
 
-    today_day_difference: function (date, ignoreToday) {
-        var today_ms = new Date().getTime();
-        var date_ms = Date.parse(date);
-        var difference_ms = Math.abs(today_ms - date_ms);
-        return Math.floor(difference_ms / XDSLA.ONE_DAY_MS) - (ignoreToday ? 1 : 0);
+    today_day_difference: function (accident_date_string) {
+        
+        var today = new Date();
+        today.setHours(0, 0, 0, 0);
+        var today_ms = today.getTime();
+
+        var accident_date = new Date(accident_date_string);
+        accident_date.setHours(0, 0, 0, 0);
+        var accident_date_ms = accident_date.getTime();
+        
+        var difference_ms = Math.abs(today_ms - accident_date_ms);
+        var return_value = Math.floor(difference_ms / XDSLA.ONE_DAY_MS) - 1; // we don't count the day of the accident
+
+        // special case if the day of the accident is today
+        if (return_value == -1) {
+            return_value = 0;
+        }
+
+        return return_value;
     },
 
     update_dashboard: function() {
-        var days = XDSLA.today_day_difference(XDSLA.last_accident, true);
+        var days = XDSLA.today_day_difference(XDSLA.last_accident);
         $('#js-number-of-days-current-number').text(days);
         $('#js-number-of-days-current-units').text((days == 1) ? 'day' : 'days');
         $('#js-person-to-blame').text(XDSLA.blame_person);
