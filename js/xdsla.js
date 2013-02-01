77f20896466e031b07ad51569000e44e415ab906
Ed Henderson <ehenderson@sugarinc.com>
2013-01-31 22:03:56 -0800

    Tested JS and made changes to get working
    - incorrect function name
    - correct quotes
    - unused mixin
    - JS tidy, refreshes once per hour

--- js/xdsla.js
+++ js/xdsla.js
@@ -3,31 +3,30 @@ var XDSLA = {
     ONE_DAY_MS: 86400000,
     last_accident: '01/29/2013',
     blame_person: 'Dino',
-    update_interval_ms: 1000,
+    update_interval_ms: 1000 * 60 * 60,
 
     init: function() {
-        startInterval();
+        XDSLA.start_interval();
     },
 
     start_interval: function() {
-        update_dashboard();
-        var interval_id = setInterval(update_dashboard, update_interval_ms);
+        XDSLA.update_dashboard();
+        var interval_id = setInterval(XDSLA.update_dashboard, XDSLA.update_interval_ms);
     },
 
     today_day_difference: function (date, ignoreToday) {
         var today_ms = new Date().getTime();
         var date_ms = Date.parse(date);
-        var difference_ms = Math.abs(today_ms - d_ms);
-        return Math.floor(difference_ms / ONE_DAY_MS) - (ignoreToday ? 1 : 0);
+        var difference_ms = Math.abs(today_ms - date_ms);
+        return Math.floor(difference_ms / XDSLA.ONE_DAY_MS) - (ignoreToday ? 1 : 0);
     },
 
     update_dashboard: function() {
-        var days = today_day_difference(last_accident, true);
+        var days = XDSLA.today_day_difference(XDSLA.last_accident, true);
         $('#js-number-of-days-current-number').text(days);
         $('#js-number-of-days-current-units').text((days == 1) ? 'day' : 'days');
-        $('#js-person-to-blame').text(blame_person);
+        $('#js-person-to-blame').text(XDSLA.blame_person);
         $('.shout').bigtext();
-        alert('yes');
     }
 
 };
